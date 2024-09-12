206ab3d2fbb2467da75b561820a4ecdb32ebb15c
diyor <diyorbekhosilov11@gmail.com>
2024-09-12 12:34:00 +0500

    Turtle race

--- main.py
+++ main.py
@@ -37,28 +37,3 @@ while is_race_on:
 
 screen.exitonclick()
 
-# def move_forward():
-#     tim.forward(10)
-#
-# def move_backward():
-#     tim.backward(10)
-#
-# def turn_left():
-#     tim.left(45)
-#
-# def turn_right():
-#     tim.right(45)
-#
-# def clear():
-#     tim.clear()
-#     tim.penup()
-#     tim.home()
-#     tim.pendown()
-#
-#
-# screen.listen()
-# screen.onkey(move_forward, "w")
-# screen.onkey(move_backward, "s")
-# screen.onkey(turn_left, "a")
-# screen.onkey(turn_right, "d")
-# screen.onkey(clear, "c")
